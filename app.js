5aeda8de013f0c2c288f832e7ba04019513a5c26
Umesh Premjee <130490125+Umesh-Seejwani@users.noreply.github.com>
2024-02-05 14:45:05 +0500

    Completed

---
+++ app.js
@@ -0,0 +1,88 @@
+// QUESTION NO 1:
+
+// var a = 10;
+// console.log("The Value of a is: " +a);
+
+// console.log("The Value of ++a is: "+(++a));
+// console.log("Now the Value of a is: "+a);
+
+// console.log("The Value of a++ is: "+(a++));
+// console.log("Now the Value of a is: "+a);
+
+// console.log("The Value of --a is: "+(--a));
+// console.log("Now the Value of a is: "+a);
+
+// console.log("The Value of a-- is: "+(a--));
+// console.log("Now the Value of a is: "+a);
+
+
+// QUESTION NO 2:
+// var a = 2;
+// var b = 1;
+
+// --a;
+
+// a was 2, but after this line, it becomes 1.
+// --a - --b;
+
+// a becomes 0, and b becomes 0.
+// So, the expression evaluates to 0 - 0.
+// --a - --b + ++b;
+
+// a becomes -1, and b becomes 1.
+// The expression now evaluates to -1 - 0 + 1.
+// --a - --b + ++b + b--;
+
+// a becomes -2, and b becomes 0.
+// The expression now evaluates to -2 - 0 + 0 + 1.
+
+// console.log("The Value of a = -2");
+// console.log("The Value of b = 1");
+// console.log("The Result is = -1")
+
+
+// QUESTION NO 3:
+// var user = prompt("Enter Your Name:");
+// console.log("Welcome to our Website "+user);
+
+
+// QUESTION NO 4:
+// var num = +prompt("Enter a Number:");
+// var num1 = 5;
+// if(num){
+//  for(var i=1; i<=10; i++){
+//     console.log(num+" * " + i +" = "+ i*num);
+//  }
+// }
+
+// else if(num1){
+//     for(var i=1; i<=10; i++){
+//         console.log(num1+" * " + i +" = "+ i*num1);
+//      }
+// }
+
+
+
+// QUESTION NO 5:
+// var subject1 = prompt("Enter your Subject 1 :");
+// var subject2 = prompt("Enter your Subject 2 :");
+// var subject3 = prompt("Enter your Subject 3 :");
+// console.log(subject1+" " +subject2+" " +subject3);
+
+
+
+// var obtainedMarks1 = +prompt("Enter Subject 1 Obtained marks :");
+// var obtainedMarks2 = +prompt("Enter Subject 2 Obtained marks :");
+// var obtainedMarks3 = +prompt("Enter Subject 3 Obtained marks :");
+// console.log("The Obtained marks is "+subject1+" " +obtainedMarks1+" "+subject2+" " +obtainedMarks2+" " +subject3+" " +obtainedMarks3);
+
+// var totalMarks = 100;
+
+// var total = obtainedMarks1 + obtainedMarks2 + obtainedMarks3;
+// console.log("The Total Obtained Marks is: "+total);
+// var finalMarks = (total / 300) * 100;
+// console.log("The Total Percentage is " +finalMarks+"%");
+
+
+
+
